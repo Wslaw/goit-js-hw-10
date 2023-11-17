78acb0a0d0d977b2ccf349f5e62cd57081c3d7a9
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-11-17 15:43:06 +0200

    update

--- src/index.js
+++ src/index.js
@@ -8,3 +8,13 @@ import axios from "axios";
 axios.defaults.headers.common['x-api-key'] =
   'live_wmYNT4c87b7lQ9XjUOOUpcXUeEHCw4gcRGHUbeZrxjFiNG5WNlXbDIQxPfZH9ACK';
 
+const refs = {
+  selector: document.querySelector('.breed-select'),
+  divCatInfo: document.querySelector('.divCatInfo'),
+  loader: document.querySelector('.loader'),
+  error: document.querySelector('.error'),
+}
+
+console.log(refs);
+
+const { selector, divCatInfo, loader, error } = refs;
