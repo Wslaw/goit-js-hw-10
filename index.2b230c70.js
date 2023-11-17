62459d297643e2d4aada6d4d5a3fe8e3386cb885
Wslaw <114644824+Wslaw@users.noreply.github.com>
2023-11-17 13:43:30 +0000

    Deploying to gh-pages from @ Wslaw/goit-js-hw-10@78acb0a0d0d977b2ccf349f5e62cd57081c3d7a9 🚀

---
+++ index.2b230c70.js
@@ -0,0 +1,2 @@
+!function(){console.log("Hello");fetch("".concat("https://api.thecatapi.com/v1","/breeds?api_key=").concat("live_wmYNT4c87b7lQ9XjUOOUpcXUeEHCw4gcRGHUbeZrxjFiNG5WNlXbDIQxPfZH9ACK")).then((function(o){if(console.log(o),!o.ok)throw new Error("404 not found");return o.json()})).catch((function(o){return console.log(o)}))}();
+//# sourceMappingURL=index.2b230c70.js.map
